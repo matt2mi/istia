a4983f1b13c36267abd7c310a6a2172cdcd77e39
matt2mi <2m1tema@gmail.com>
2019-10-20 14:44:26 +0200

    Update professors.ts

--- professors.ts
+++ professors.ts
@@ -1,6 +1,6 @@
 professors = [
   { firstname: 'Hugo', lastname: 'Charles', company: 'Parrot' },
   { firstname: 'Mathieu', lastname: 'DEUMIE', company: 'Sfeir' },
-  { firstname: 'Vincent', lastname: 'VOISIN', company: 'Freelance' },
-  { firstname: 'Mehdi', lastname: 'LHOMMEAU', company: 'Istia' }
+  { firstname: 'Vincent', lastname: 'VOISIN', company: '' },
+  { firstname: 'Mehdi', lastname: 'LHOMMEAU', company: '' }
 ]
