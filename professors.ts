7cc7ddccc1d97b91d902ed89d9f2601102ab7053
matt2mi <2m1tema@gmail.com>
2019-10-20 18:55:16 +0200

    Update professors.ts

--- professors.ts
+++ professors.ts
@@ -1,6 +1,6 @@
 professors = [
-  { id: 1, firstname: 'Hugo', lastname: 'Charles', company: 'Parrot', url: 'https://www.parrot.com/fr/' },
-  { id: 2, firstname: 'Mathieu', lastname: 'DEUMIE', company: 'Sfeir', url: 'https://www.sfeir.com/fr' },
+  { id: 1, firstname: 'Hugo', lastname: 'Charles', company: 'Parrot' },
+  { id: 2, firstname: 'Mathieu', lastname: 'DEUMIE', company: 'Sfeir' },
   { id: 3, firstname: 'Vincent', lastname: 'VOISIN', company: '' },
-  { id: 4, firstname: 'Mehdi', lastname: 'LHOMMEAU', company: '', url: 'http://www.polytech-angers.fr/fr/index.html' }
+  { id: 4, firstname: 'Mehdi', lastname: 'LHOMMEAU', company: '' }
 ]
