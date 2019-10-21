2a67e10c2319f127fa19bfffdedeaa0a945437f9
matt2mi <2m1tema@gmail.com>
2019-10-22 01:01:38 +0200

    Update and rename professors.ts to youtubers.ts

--- professors.ts
+++
@@ -1,6 +0,0 @@
-professors = [
-  { id: 1, firstname: 'Hugo', lastname: 'Charles', company: 'Parrot' },
-  { id: 2, firstname: 'Mathieu', lastname: 'DEUMIE', company: 'Sfeir' },
-  { id: 3, firstname: 'Vincent', lastname: 'VOISIN', company: '' },
-  { id: 4, firstname: 'Mehdi', lastname: 'LHOMMEAU', company: '' }
-]
